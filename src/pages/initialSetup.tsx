26568beb835b3d77db1f3bee1a5d0c51d529ac4d
AmritpalChera <cheraamritpal@gmail.com>
2023-07-11 18:37:56 -0400

    add loading state

--- src/pages/initialSetup.tsx
+++ src/pages/initialSetup.tsx
@@ -1,6 +1,6 @@
 import supabase from '@/utils/setup/supabase';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setUserData } from '@/redux/features/UserSlice';
 import { protectedRoutes } from '@/utils/misc';
@@ -12,8 +12,10 @@ interface SetupProps {
 export default function Setup({children}: SetupProps) {
   const dispatch = useDispatch();
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const userSession1 = async () => {
     const routename = router.pathname.toLowerCase();
+    if (routename !== '/') setLoading(true);
     const session = await supabase.auth.getSession();
     // const customer = await supabase.from('customers').select().single();
     const user = session?.data?.session?.user;
@@ -24,7 +26,7 @@ export default function Setup({children}: SetupProps) {
       dispatch(setUserData(dataToSet));
       const signinRedirect = localStorage.getItem('signinRedirect');
       localStorage.removeItem('signinRedirect');
-
+      setLoading(false);
       if (signinRedirect) {
         router.push(signinRedirect);
         return;
@@ -33,9 +35,11 @@ export default function Setup({children}: SetupProps) {
       // user does not exists, see if they're trying to access protected route
       if (protectedRoutes.includes(routename)) {
         localStorage.setItem('signinRedirect', routename);
+        setLoading(false);
         router.push('/login');
       }
     }
+    setLoading(false);
     // Track an event. It can be anything, but in this example, we're tracking a Signed Up event.
     // Include a property about the signup, like the Signup Type
   }
@@ -43,6 +47,14 @@ export default function Setup({children}: SetupProps) {
   useEffect(() => {
     userSession1();
   }, [supabase, router]);
+
+  if (localStorage.getItem('signinRedirect')) {
+    return (
+      <div className='flex w-full h-full justify-center items-center'>
+        Loading...
+      </div>
+    )
+  }
 
   return (
     children
